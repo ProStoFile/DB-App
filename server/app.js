81208a61e263222fb8812f9ce624b6da69242862
Aleksey Andreev <fistashki22@gmail.com>
2022-12-03 20:41:15 +0300

    create database

--- server/app.js
+++ server/app.js
@@ -2,8 +2,29 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
+const { request, response } = require('express');
 dotenv.config();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// CREATE
+
+app.post('/insert', (request, response) => {
+
+});
+
+// READ
+
+app.get('/getAll', (request, response) => {
+    response.json({
+        success: true
+    });
+})
+
+// UPDATE
+
+// DELETE
+
+app.listen(process.env.PORT, () => console.log('app is running'))
